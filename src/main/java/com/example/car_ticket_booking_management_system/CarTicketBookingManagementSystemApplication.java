a0df1d2172f16117f2f7dc4f74cb09cfebf60e27
arafatrimon <rimonarafat2020@gmail.com>
2023-02-28 17:57:16 +0600

    first commit

---
+++ src/main/java/com/example/car_ticket_booking_management_system/CarTicketBookingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_ticket_booking_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarTicketBookingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarTicketBookingManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/car_ticket_booking_management_system/CarTicketBookingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_ticket_booking_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarTicketBookingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
